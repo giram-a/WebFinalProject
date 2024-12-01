742e7d6ec743b8c4fee9b1e6641659c0f4008b1e
Amey Satwe <amey10051999@gmail.com>
2024-12-01 12:32:49 -0500

    [Employer flow] Company can't add jobs without approval, Added a dialog with logout button.

--- frontend/src/pages/Employer/AddJob.jsx
+++ frontend/src/pages/Employer/AddJob.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
@@ -7,38 +7,22 @@ import { Textarea } from "@/components/ui/textarea"
 import { useToast } from '@/hooks/use-toast.js'
 import { Toaster } from '@/components/ui/toaster'
 import { createJob } from '@/api/jobsApi'
-import { useAuth, useUser } from '@clerk/clerk-react'
+import { useAuth, useUser, useClerk } from '@clerk/clerk-react'
 import { findCompany } from '@/api/companyApi'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
 
 const AddJob = () => {
   const { getToken } = useAuth();
-  const { user,isLoaded } = useUser();
-
-  
-    useEffect(() => {
-      if (isLoaded && user) {
-        fetchDataForCompany();
-      }
-    }, [isLoaded, user]);
-    
-    const fetchDataForCompany = async () => {
-      try {
-      console.log(user)
-      let token = await getToken();
-      const res = await findCompany(user.id, token);
-      // console.log(res.data)
-      if (res.data.accessStatus === "PENDING") {
-        alert("approval needed")
-      }
-    } catch (error) {
-      console.error("Error fetching company data:", error);
-      toast({
-        title: "Error",
-        description: "Failed to fetch company data. Please try again.",
-        variant: "destructive",
-      });
-    }
-  };
+  const { user, isLoaded } = useUser();
+  const { signOut } = useClerk();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const initialFormState = {
     companyName: "",
@@ -53,6 +37,30 @@ const AddJob = () => {
   const [formData, setFormData] = useState(initialFormState);
 
   const { toast } = useToast();
+
+  useEffect(() => {
+    if (isLoaded && user) {
+      fetchDataForCompany();
+    }
+  }, [isLoaded, user]);
+    
+  const fetchDataForCompany = async () => {
+    try {
+      console.log(user)
+      let token = await getToken();
+      const res = await findCompany(user.id, token);
+      if (res.data.accessStatus === "PENDING") {
+        setIsDialogOpen(true);
+      }
+    } catch (error) {
+      console.error("Error fetching company data:", error);
+      toast({
+        title: "Error",
+        description: "Failed to fetch company data. Please try again.",
+        variant: "destructive",
+      });
+    }
+  };
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
@@ -79,88 +87,112 @@ const AddJob = () => {
     }
   };
 
+  const handleLogout = async () => {
+    await signOut();
+    // Redirect to login page or home page after logout
+    // You might want to use react-router's useNavigate hook for this
+    // navigate('/login');
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <Toaster />
-      <Card className="w-full max-w-2xl mx-auto border shadow-none">
-        <CardHeader>
-          <CardTitle>Job Posting Form</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex md:flex-nowrap justify-between gap-2 xs:flex-wrap">
-            <div className="space-y-2 w-full">
-              <Label htmlFor="companyName">Company Name *</Label>
-              <Input id="companyName"
-                placeholder="Enter company name"
-                value={formData.companyName}
-                onChange={handleInputChange} required />
+    <>
+      <form onSubmit={handleSubmit}>
+        <Toaster />
+        <Card className="w-full max-w-2xl mx-auto border shadow-none">
+          <CardHeader>
+            <CardTitle>Job Posting Form</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex md:flex-nowrap justify-between gap-2 xs:flex-wrap">
+              <div className="space-y-2 w-full">
+                <Label htmlFor="companyName">Company Name *</Label>
+                <Input id="companyName"
+                  placeholder="Enter company name"
+                  value={formData.companyName}
+                  onChange={handleInputChange} required />
+              </div>
+              <div className="space-y-2 w-full">
+                <Label htmlFor="jobTitle">Job Title *</Label>
+                <Input id="jobTitle"
+                  placeholder="Enter job title"
+                  value={formData.jobTitle}
+                  onChange={handleInputChange}
+                  required />
+              </div>
             </div>
-            <div className="space-y-2 w-full">
-              <Label htmlFor="jobTitle">Job Title *</Label>
-              <Input id="jobTitle"
-                placeholder="Enter job title"
-                value={formData.jobTitle}
-                onChange={handleInputChange}
-                required />
-            </div>
-          </div>
-          <div className="space-y-2 mt-2">
-            <Label htmlFor="description">Description *</Label>
-            <Textarea
-              id="description"
-              placeholder="Enter job description"
-              value={formData.description}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div className="flex flex-wrap justify-between gap-2 md:flex-nowrap xs:flex-wrap mt-2">
-            <div className="space-y-2 w-full">
-              <Label htmlFor="salary">Salary *</Label>
-              <Input
-                id="salary"
-                placeholder="Enter salary"
-                value={formData.salary}
+            <div className="space-y-2 mt-2">
+              <Label htmlFor="description">Description *</Label>
+              <Textarea
+                id="description"
+                placeholder="Enter job description"
+                value={formData.description}
                 onChange={handleInputChange}
                 required
               />
             </div>
-            <div className="space-y-2 w-full">
-              <Label htmlFor="location">Location (Optional)</Label>
-              <Input
-                id="location"
-                placeholder="Enter job location"
-                value={formData.location}
+            <div className="flex flex-wrap justify-between gap-2 md:flex-nowrap xs:flex-wrap mt-2">
+              <div className="space-y-2 w-full">
+                <Label htmlFor="salary">Salary *</Label>
+                <Input
+                  id="salary"
+                  placeholder="Enter salary"
+                  value={formData.salary}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="space-y-2 w-full">
+                <Label htmlFor="location">Location (Optional)</Label>
+                <Input
+                  id="location"
+                  placeholder="Enter job location"
+                  value={formData.location}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+            <div className="space-y-2 mt-2">
+              <Label htmlFor="skills">Required Skills (Optional)</Label>
+              <Textarea
+                id="skills"
+                placeholder="Enter required skills"
+                value={formData.skills}
                 onChange={handleInputChange}
               />
             </div>
-          </div>
-          <div className="space-y-2 mt-2">
-            <Label htmlFor="skills">Required Skills (Optional)</Label>
-            <Textarea
-              id="skills"
-              placeholder="Enter required skills"
-              value={formData.skills}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="space-y-2 mt-2">
-            <Label htmlFor="apply-link">Application Link (Optional)</Label>
-            <Input
-              id="applyLink"
-              type="url"
-              placeholder="Enter application URL"
-              value={formData.applyLink}
-              onChange={handleInputChange}
-            />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button type="submit" className="w-full">Submit Job Posting</Button>
-        </CardFooter>
-      </Card>
-    </form>
+            <div className="space-y-2 mt-2">
+              <Label htmlFor="apply-link">Application Link (Optional)</Label>
+              <Input
+                id="applyLink"
+                type="url"
+                placeholder="Enter application URL"
+                value={formData.applyLink}
+                onChange={handleInputChange}
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button type="submit" className="w-full">Submit Job Posting</Button>
+          </CardFooter>
+        </Card>
+      </form>
+
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen} hideCloseButton>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Approval Required</DialogTitle>
+            <DialogDescription>
+              Your company's access status is pending. Please contact the admin for approval. You will be logged out from here.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button onClick={handleLogout} variant="destructive" className="w-full">Logout</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
 
 export default AddJob
+
